cf78cc62e33c50cee96c83f613ec10741be3661a
elleyjin <95072132+elleyjin@users.noreply.github.com>
2023-09-23 12:56:14 +0800

    Added js script to change location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 let apiKey = "d0138a4c7d1cd8871d6ba4c962225917";
+
+function showCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  // let searchCity = document.querySelector("#")
+  let currentLocation = document.querySelector("#location-text");
+  currentLocation.innerHTML = searchInput.value;
+}
+
+let searchButton = document.querySelector("#search-btn");
+searchButton.addEventListener("click", showCity);
