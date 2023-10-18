3d4ba92e9b70ca24f51ccbc6717ae558d974525a
elleyjin <95072132+elleyjin@users.noreply.github.com>
2023-10-18 11:07:52 +0800

    fixed minor layout css

--- src/index.js
+++ src/index.js
@@ -74,7 +74,6 @@ function switchIcon(icon) {
 }
 
 function getForecastApi(coordinates) {
-  console.log(coordinates);
   let apiKey = "46fac47dd8b8fa26d1b6852218ad3dfe";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showForecast);
@@ -117,8 +116,6 @@ function showForecast(response) {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-
-  console.log(forecast);
 }
 
 function formatDay(timestamp) {
